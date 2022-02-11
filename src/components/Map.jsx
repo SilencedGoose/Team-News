88ea13e90c5ccd67eb915bf8da68b21d1d2ac4f4
AnimatorOfSouls <fantailfishy@gmail.com>
2022-02-11 20:01:48 +0000

    Map loads now

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
-export default function Map() {
+export default function MapHandler() {
+	function loadMap() {
+		var map = L.map('map').setView([51.505, -0.09], 13);
+		
+		L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+			attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+			maxZoom: 18,
+			id: 'mapbox/streets-v11',
+			tileSize: 512,
+			zoomOffset: -1,
+			accessToken: 'pk.eyJ1IjoiYW5pbWF0b3JvZnNvdWxzIiwiYSI6ImNreml0eXNtMTFvYWwycW5yY2VhaHI2MDAifQ.ZSNNSGLF4DixEuS3RrLtHQ'
+		}).addTo(map);
+	}
 	return (
-		<div>Hi</div>
+		<div>
+			<button onClick={loadMap}>Load Map</button>
+			
+			<div id="map" onload="alert('egg')"></div>
+		</div>
 	)
 }
