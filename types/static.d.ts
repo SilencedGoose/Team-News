f52b5ee3a04f814fd45ba8516950b28946da04ed
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2022-02-12 12:44:18 +0000

    updated Map component
    introduced typing, fixed so that it loads on page load, and some minor styling tweaks

--- types/static.d.ts
+++ types/static.d.ts
@@ -2,8 +2,14 @@ interface articleProps {
   headline: string;
   url: string;
   img: string;
+  alt: string;
 }
 
 interface newsProps {
   data: articleProps[];
+}
+
+interface user {
+  name: string;
+  location: LatLngExpression;
 }
